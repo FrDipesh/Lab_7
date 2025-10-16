4b1331d4447ab4cac8f3e85b3d4572dbe1bb043c
618989 <poudel.618989@swl.k12.oh.us>
2025-10-16 08:54:21 -0400

    Task1

---
+++ src/Lab7Task1.java
@@ -0,0 +1,18 @@
+public class Lab7Task1 {
+    public static void main(String[] args) {
+        int countUp = 0;
+        int countDown = 0;
+        int countBy3 = 0;
+        int countBy2 = 0;
+
+        System.out.println("Count from 0-30");
+        for(int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nCount from 30-0");
+        for(int j = 30; j >= 0; j--) {
+            System.out.println(j);
+        }
+    }
+}
