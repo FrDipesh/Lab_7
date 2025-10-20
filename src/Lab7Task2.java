4ecf0ded5904f4446b82b822abeda3413e506299
618989 <poudel.618989@swl.k12.oh.us>
2025-10-20 08:55:16 -0400

    Done with Task 1

---
+++ src/Lab7Task2.java
@@ -0,0 +1,31 @@
+public class Lab7Task2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 5; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 5; i <= 5 i++) {
+            String currentLetter = s.substring(i, i+1);
+            for (int j = 1; j <= n; j++) {
+                System.out.print(currentLetter);
+            }
+            System.out.println();
+        }
+
+    }
+}
